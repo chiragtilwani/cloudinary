c48dc2a0c9ae24b478144af7861cce363f8788b9
Chirag Tilwani <73477473+chiragtilwani@users.noreply.github.com>
2023-01-25 22:59:29 +0530

    Create cloudinary.js

---
+++ cloudinary.js
@@ -0,0 +1,11 @@
+//create middleware/cloudinary
+
+require("dotenv").config();
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
+})
+
+module.exports = cloudinary
